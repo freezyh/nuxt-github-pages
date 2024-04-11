c71fd636e883b339504d7c05fca136a89182489c
zhongyuanhui <2294963715@qq.com>
2024-04-11 17:13:37 +0800

    feat: change public path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ const path = require('path');
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/nuxt-github-pages/'
+  }, 
   nitro: {
     output: {
       publicDir: path.join(__dirname, 'docs')
