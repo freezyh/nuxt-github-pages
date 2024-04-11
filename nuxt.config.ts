bfa99d8ca2e3f335ebb60035b41100d40489b359
zhongyuanhui <2294963715@qq.com>
2024-04-11 17:07:42 +0800

    feat: output folder change to be dosc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
+const path = require('path');
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs')
+    }
+  }
 })
